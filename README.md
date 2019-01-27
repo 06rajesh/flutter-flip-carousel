4b8b6133efd6a0e732ad3f1fb588ce3fecc47439
rajesh06 <abhi.crushefix@gmail.com>
2019-01-27 18:36:40 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# card_flip_carousel
+
+Implementing Card Flip Carousel in Flutter
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
+# flutter-flip-carousel
---
+++ lib/main.dart
@@ -0,0 +1,328 @@
+import 'dart:ui';
+
+import 'package:card_flip_carousel/card_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main(){
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flip Carousel',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.black,
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          // Room for status bar
+          new Container(
+            width: double.infinity,
+            height: 20.0,
+          ),
+
+          // Cards
+          new Expanded(
+            child: new CardFlipper(
+              cards: demoCards,
+            )
+          ),
+
+          // Bottom Bar
+          new Container(
+            width: double.infinity,
+            height: 50.0,
+            color: Colors.grey,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CardFlipper extends StatefulWidget {
+  final List<CardViewModel> cards;
+
+  CardFlipper({
+    this.cards,
+  });
+
+  @override
+  _CardFlipperState createState() => _CardFlipperState();
+}
+
+class _CardFlipperState extends State<CardFlipper> with TickerProviderStateMixin{
+
+  double scrollPercent = 0.0;
+  Offset startDrag;
+  double startDragPercentScroll;
+  double finishScrollStart;
+  double finishScrollEnd;
+  AnimationController finishScrollController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    finishScrollController = new AnimationController(
+      duration: const Duration(milliseconds: 150),
+      vsync: this,
+    )
+    ..addListener((){
+      setState((){
+        scrollPercent = lerpDouble(finishScrollStart, finishScrollEnd, finishScrollController.value);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    finishScrollController.dispose();
+    super.dispose();
+  }
+
+  void _onHorizontalDragStart(DragStartDetails details){
+    startDrag = details.globalPosition;
+    startDragPercentScroll = scrollPercent;
+  }
+
+  void _onHorizontalDragUpdate(DragUpdateDetails details){
+    final currDrag = details.globalPosition;
+    final dragDistance = currDrag.dx - startDrag.dx;
+    final singleCardDragPercent = dragDistance / context.size.width;
+
+    final numCards = 3;
+    setState(() {
+      scrollPercent = (startDragPercentScroll + (-singleCardDragPercent / numCards))
+          .clamp(0.0, 1.0 - (1/numCards));
+    });
+  }
+
+  void _onHorizontalDragEnd(DragEndDetails details){
+    finishScrollStart = scrollPercent;
+    finishScrollEnd = (scrollPercent * widget.cards.length).round() / widget.cards.length;
+    finishScrollController.forward(from: 0.0);
+
+    setState(() {
+      startDrag = null;
+      startDragPercentScroll = null;
+    });
+  }
+
+  List<Widget> _buildCards(){
+    final cardCount = widget.cards.length;
+
+    int index = -1;
+    return widget.cards.map((CardViewModel viewModel){
+      ++index;
+      return _buildCard(viewModel, index, cardCount, scrollPercent);
+    }).toList();
+  }
+
+  Widget _buildCard(CardViewModel viewModel, int cardIndex, int cardCount, double scrollPercent){
+    final cardScrollPercent = scrollPercent / (1 / cardCount);
+    final parallax = scrollPercent - (cardIndex / cardCount);
+
+    return new FractionalTranslation(
+      translation: new Offset(cardIndex - cardScrollPercent, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: new Card(
+          viewModel: viewModel,
+          parallaxPercent : parallax
+        ),
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: _onHorizontalDragStart,
+      onHorizontalDragUpdate: _onHorizontalDragUpdate,
+      onHorizontalDragEnd: _onHorizontalDragEnd,
+      behavior: HitTestBehavior.translucent,
+      child: new Stack(
+        children: _buildCards()
+      ),
+    );
+  }
+}
+
+
+class Card extends StatelessWidget {
+
+  final CardViewModel viewModel;
+  final double parallaxPercent;
+
+  Card({
+    this.viewModel,
+    this.parallaxPercent
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        //Background
+        new ClipRRect(
+          borderRadius: new BorderRadius.circular(10.0),
+          child: FractionalTranslation(
+            translation: new Offset(parallaxPercent * 2.0, 0.0),
+            child: OverflowBox(
+              maxWidth: double.infinity,
+              child: new Image.asset(
+                viewModel.backdropAssetPath,
+                fit: BoxFit.cover
+              ),
+            ),
+          ),
+        ),
+
+        // Content
+        new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+              child: new Text(
+                viewModel.address.toUpperCase(),
+                style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontFamily: 'petita',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0
+                ),
+              ),
+            ),
+            new Expanded(child: new Container()),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(
+                  '${viewModel.minHeightInFeet} - ${viewModel.maxHeightInFeet}',
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 140.0,
+                    fontFamily: 'petita',
+                    letterSpacing: -5.0
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, top: 30.0),
+                  child: new Text(
+                    'FT',
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 22.0,
+                      fontFamily: 'petita',
+                      fontWeight: FontWeight.bold,
+
+                    ),
+                  ),
+                )
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Icon(
+                  Icons.wb_sunny,
+                  color: Colors.white,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: new Text(
+                    '${viewModel.tempInDegrees}°',
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'petita',
+                      fontWeight:  FontWeight.bold,
+                      fontSize: 20.0
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            new Expanded(child: new Container()),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0, bottom: 50.0),
+              child: new Container(
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.circular(30.0),
+                  border: new Border.all(
+                    color: Colors.white,
+                    width: 1.5
+                  ),
+                  color: Colors.black.withOpacity(0.3)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new Text(
+                        viewModel.weatherType,
+                        style: new TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'petita',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0
+                        ),
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                        child: new Icon(
+                          Icons.wb_cloudy,
+                          color: Colors.white,
+                        ),
+                      ),
+                      new Text(
+                        '${viewModel.windSpeedInMph}mph ${viewModel.cardinalDirection}',
+                        style: new TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'petata',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
